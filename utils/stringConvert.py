53446b776b9c7ea603fb64549d5817ea3f488938
kseilons <safonovandrey00@list.ru>
2023-07-31 11:12:26 +0300

    delete config and device

--- utils/stringConvert.py
+++ utils/stringConvert.py
@@ -1,3 +1,4 @@
+import string
 def str_to_bool(s):
     if s == '1':
         return True
@@ -5,3 +6,9 @@ def str_to_bool(s):
         return False
     else:
         raise ValueError("Cannot convert string to bool")
+
+def str_to_int(s):
+    if s.lower() == 'false':
+        return 0
+    if s.lower() == 'true':
+        return 1
